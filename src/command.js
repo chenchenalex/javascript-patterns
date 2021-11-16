7ea2896367b2ba06ebef8ba9d9b9a93852fa7998
NJ <chenchenalex1@gmail.com>
2021-11-16 12:20:52 +1100

    Update command.js

--- src/command.js
+++ src/command.js
@@ -1,33 +1,44 @@
 //  You need a command to have a life span independent of the original request
 
-var carManager = {
-  // request information
-  requestInfo: function (model, id) {
-    return "The information for " + model + " with ID " + id + " is foobar";
+const clipboard = {
+  clipboard: '',
+  isCtrl: false,
+  isShift: true,
+  commands: {
+    '67': this.copy, // pressed C
+    '68': this.duplicate, // pressed D
   },
+  history: [],
+  duplicate () {},
+  copy(text){
+    clipboard = text;
+    this.history.push({
+      command: 'copy',
+      payload: text
+    })
 
-  // purchase the car
-  buyVehicle: function (model, id) {
-    return "You have successfully purchased Item " + id + ", a " + model;
+    if(isSelectedType === 'text'){ textCopy() }
+    if(isSelectedType === 'screen'){ screenCopy()}
+    if(isSelectedType === 'component'){ componentCopy()}
   },
+  undo(){
 
-  // arrange a viewing
-  arrangeViewing: function (model, id) {
-    return "You have successfully booked a viewing of " + model + " ( " + id + " ) ";
   },
-};
+  redo(){
 
-/* usually we like to directly call the method, but if the shape of requestInfo changed
-  everywhere needs to be updated
-*/
-// there an alternative way...
-carManager.execute = function (name) {
-  // adds the command to history for undo / redo
-  carManager.history.push(name);
-  return carManager[name] && carManager[name].apply(carManager, [].slice.call(arguments, 1));
-};
+  }
+}
 
-carManager.execute("buyVehicle", "Ford Escort", "453543");
-carManager.execute("requestInfo", "Ford Mondeo", "54323");
-carManager.execute("arrangeViewing", "Ford Escort", "34232");
-carManager.execute("unknownCommand", "xxx", "yyy"); // it does not break,
+// scenario 1
+copyButton.onClick((e) => {
+  clipboard.copy(e.target.value);
+})
+
+// scenario 2
+contextMenu.onClick((e)=> {
+  clipboard.copy(this.value);
+})
+
+document.on('CUSTOM_COPY_EVENT', (e){
+  clipboard.copy(e)
+})
